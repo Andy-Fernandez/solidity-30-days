61d25d0e8a7acc9d78469ba01b162de6e647d736
Andy-Fernandez <andyfer282810@gmail.com>
2025-05-31 11:28:58 -0400

    Challenge 1 complete!

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -4,11 +4,23 @@ pragma solidity ^0.8.28;
 contract Counter {
     uint public counter;
 
-    constructor(uint startNumber) public {
+    event CounterIncrement(uint newValue);
+    event CounterDecrement(uint newValue);
+
+    constructor(uint startNumber) {
+      require(startNumber >= 0, "Counter needs a positive starting number");
       counter = startNumber;
     }
 
-    function addCounter() public returns(uint) {
+    function incrementr() public returns(uint) {
+      emit CounterIncrement(counter);
       return counter += 1;
+    }
+
+    function decrement() public returns(uint) {
+      // we should add and require!
+      require(counter > 0 , "Cant decrement less than zero");
+      emit CounterDecrement(counter);
+      return counter -= 1;
     }
 }
