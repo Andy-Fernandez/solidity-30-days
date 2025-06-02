3125aeb3da2406eddd0c1a0e6d401bb6fc079dc6
Andy-Fernandez <andyfer282810@gmail.com>
2025-06-02 12:20:45 -0400

    Ejercicio día 2 terminado :)

---
+++ contracts/SaveMyName.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract SaveMyName {
+    string public name; // ya tenemos por defencto el view
+    string public description; // same
+    address public owner;
+
+    constructor(string memory _name, string memory _description) {
+        name = _name;
+        description= _description;
+        owner = msg.sender;
+    }
+
+    function setName(string memory _newName) external {
+        require(msg.sender == owner, "You aren't the owner");
+        name = _newName;
+    }
+}
