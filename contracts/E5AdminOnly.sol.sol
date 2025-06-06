d4b851d7e0f85b1723bbd3dc1b8a3911f30d4e7a
Andy-Fernandez <andyfer282810@gmail.com>
2025-06-06 10:00:45 -0400

    Exercise 5 done!

--- contracts/E5AdminOnly.sol.sol
+++ contracts/E5AdminOnly.sol.sol
@@ -2,42 +2,76 @@
 pragma solidity ^0.8.27;
 
 contract AdminOnly {
-    
-    // uint treasure = 0; // No es necesaria esta linea por que el contrato mismo es el que guarda todas las transacciones
-    address owner;
+    address public owner;
 
-    mapping (address => bool) whitelist;
+    mapping(address => bool) public whitelist;
+    mapping(address => bool) public hasWithdrawn;
+    mapping(address => uint) public withdrawalPermit;
 
-    constructor (){
+    event TreasureDeposited(address indexed user, uint amount);
+    event Withdrawn(address indexed user, uint amount);
+
+    constructor() {
         owner = msg.sender;
     }
 
-    function deposit(uint amount) external  {
+    // Receive ETH
+    receive() external payable {
+        emit TreasureDeposited(msg.sender, msg.value);
+    }
+
+    function deposit() external payable {
+        emit TreasureDeposited(msg.sender, msg.value);
+    }
+
+    function withdraw(uint amount) public onlyOwner onlyWhitelist notWithdrawn {
+        require(address(this).balance >= amount, "Insufficient funds!");
+        require(amount <= withdrawalPermit[msg.sender], "Not approved to withdraw this amount");
+
+        hasWithdrawn[msg.sender] = true;
+        withdrawalPermit[msg.sender] = 0; // Optional: reset
+        payable(msg.sender).transfer(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function ownerWithdraw(uint amount) public onlyOwner {
+        require(address(this).balance >= amount, "Insufficient funds!");
         payable(msg.sender).transfer(amount);
     }
 
-    function whitdraw(uint amount) public  onlyOwner {
-        require (treasure < amount, "Insufficient funds!");
-
-
-        payable(msg.sender).transfer(amount);
-        
-        treasure += amount;
-        return true;
+    function addWhitelist(address user) public onlyOwner {
+        whitelist[user] = true;
     }
 
+    function setWithdrawalPermit(address user, uint amount) public onlyOwner {
+        withdrawalPermit[user] = amount;
+    }
 
-    /* --------------- */
-    /* MODIFIERS       */
-    /* --------------- */ 
+    function resetWithdrawal(address user) public onlyOwner {
+        hasWithdrawn[user] = false;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Invalid address");
+        owner = newOwner;
+    }
+
+    function getTreasureBalance() external view returns (uint) {
+        return address(this).balance;
+    }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "You are not the owner");
         _;
     }
 
-    modifier onlyWhitelist(){
+    modifier onlyWhitelist() {
         require(whitelist[msg.sender], "Not whitelisted...");
-        _; 
+        _;
+    }
+
+    modifier notWithdrawn() {
+        require(!hasWithdrawn[msg.sender], "You already withdrew.");
+        _;
     }
 }
