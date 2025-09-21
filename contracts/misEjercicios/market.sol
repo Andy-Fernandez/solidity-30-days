0e4931e1019dad36b5253254bfd3458d965cf509
Andy-Fernandez <andyfer282810@gmail.com>
2025-09-20 21:09:24 -0400

    Estructurando el ejercicio

--- contracts/misEjercicios/market.sol
+++ contracts/misEjercicios/market.sol
@@ -11,4 +11,29 @@ contract decentraliceMarketplace {
         owners[msg.sender] += msg.value;
         emit DepositoRealizado(msg.sender, msg.value);
     }
+
+    /*  Se supone que las personas que envían X cantidad de ETH son
+        deuñas en un porcentaje del proyecto y deberíamos poder
+        ver en que porcentaje son dueñas.
+    */
+    function checkMyPorcentOwn() public view returns(uint){
+        return address(this).balance / owners[msg.sender];
+    }
 }
+
+/*
+    Tenemos:
+        1) Tenemos varios owners.
+        2) Cada uno es dueño de un porcenje del proyecto.
+        3) El dinero que se recolecta va a tener una ganancia:
+            3.1) Con este dinero se pueden hacer diferentes operaciones,
+                 el punto es que este dinero genera GANANCIA o PERDIDA.
+        4) Las ganancias o perdidas de distribuyen entre los dueños del
+           proyecto.
+        5) Cada usuario decide si sacar el dinero, reinvertirlo o sacarlo.
+            5.1) En caso de ganancia, el dinero no se reinvierte automaticamente,
+                 más bien se queda en un estado de retirado pendiente.
+            5.2) En caso de perdida, el dinero se descuenta automaticamente.
+        Preguntas: ¿Sería ideal crear otro token que represente el valor
+                    que tenemos invertido en el contrato?
+*/
