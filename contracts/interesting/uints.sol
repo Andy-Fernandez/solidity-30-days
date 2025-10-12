7122149a817fc5dfe2ce9508baff9a2bf9f28817
Andy-Fernandez <andyfer282810@gmail.com>
2025-10-12 14:24:05 -0400

    Examples about how to use uints

---
+++ contracts/interesting/uints.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+/*
+    When undestand how uints works and when use them. 
+*/
+
+contract uints {
+    uint8 public myUint8; // uint8 -> 2^8 = 256 
+    uint16 public myUint16; // uint16 -> 2^16 = 65,536
+    uint32 public myUint32;
+    uint64 public myUint64;
+    uint128 public myUint128;
+    uint256 public myUint256;
+
+    // uint8 public constant COOLDOWN = 1 days; // 24*60*60 = 86,400 seconds 
+
+}
